37e3df85aaaa9b21629a0776a597f420e9211b91
andmansim <91722324+andmansim@users.noreply.github.com>
2021-12-03 13:14:35 +0100

    suma de los elementos

--- suma_simple_de_una_matriz.py
+++ suma_simple_de_una_matriz.py
@@ -1,23 +1,26 @@
-#matriz = [["", " ", ""],
-          #["", "", ""], 
-          #["", "", ""]]
-
 import random
-   
+  
 matriz =[]
 
 def creacionmatriz(n,m):
         fila = []
+        h = 0
         for i in range(n,m): #linea
             for j in range(n,m): #columna
                 
                 a = random.randint(0,100)
                 fila.append(a)
-                
+                h = h + a 
             matriz.append (fila)
             fila = []
-           
-creacionmatriz(0,3)
- 
+            
+        return(h)
+suma = creacionmatriz(0,3)  # Dimensiones de la matriz y donde nos añade el valor final
+
+# Para unir las listas que forma a la principal
+
 for x in matriz:
     print(" ".join(str(x)))
+
+print(suma)
+
