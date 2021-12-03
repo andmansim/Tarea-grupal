3b08fbce9f5a9b2480b520771811895c78ad0e9b
andmansim <91722324+andmansim@users.noreply.github.com>
2021-12-03 14:08:15 +0100

    reducido

--- la_escalera.py
+++ la_escalera.py
@@ -10,7 +10,22 @@ lo que hay que conseguir:
 import random
 #n = random.randint(0,10)
 #print (n)
-n = 3
+
+m = 3
+for b in range(0,3):
+    if m!=b:
+        suma ='#' * b
+        print(suma)
+    
+    
+    if m == b:
+        suma1 = '#' * m
+        print(suma1)
+        
+
+    
+
+"""
 if n != 0:
     print ('#' * (n-n))
 if n != 1:
@@ -18,5 +33,5 @@ if n != 1:
 if n != 2:
     print ('#' * (n-(n-2)))
 if n == 3:
-    print ('#' * (n-(n-3)))
+    print ('#' * (n-(n-3))) """
 
