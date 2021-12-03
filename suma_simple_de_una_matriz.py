003ff1b5978a4eadd111ba7aec62e04ef50579ea
andmansim <91722324+andmansim@users.noreply.github.com>
2021-12-03 10:11:48 +0100

    creación matriz

--- suma_simple_de_una_matriz.py
+++ suma_simple_de_una_matriz.py
@@ -1,5 +1,22 @@
 #matriz = [["", " ", ""],
           #["", "", ""], 
           #["", "", ""]]
+matriz = []
+posiciones = ((0,0), (0,1), (0,2), (1,0), (1,1), (1,2), (2,0), (2,1), (2,2))
+def creacionmatriz():
+    fila = []
+    for i in range (0,2):
+        for j in range (0,2):
+            if tuple([i,j]) in posiciones:
+                fila.append (" ")
+        
+        matriz.append (fila)
+        matriz = []
+
+creacionmatriz()
+
+for x in matriz:
+    print(",".join(x))
+    
 
 
