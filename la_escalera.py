ac8eaaf83ed35e24065a997a2be67ba42c60639a
andmansim <91722324+andmansim@users.noreply.github.com>
2021-12-03 14:15:55 +0100

    terminado

--- la_escalera.py
+++ la_escalera.py
@@ -5,13 +5,10 @@ lo que hay que conseguir:
 ###
 ####
 #####
-... hasta n
+... hasta m
 """
 import random
 m = random.randint(1,10)
 for b in range(0,m+1):
-    suma ='#' * b
-    print(suma)
-    
-    
-      
+    producto ='#' * b
+    print(producto)
