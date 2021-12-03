548a1b4486751727f2839307e3ff444ea940e184
andmansim <91722324+andmansim@users.noreply.github.com>
2021-12-03 10:49:27 +0100

    mejora

--- suma_simple_de_una_matriz.py
+++ suma_simple_de_una_matriz.py
@@ -3,23 +3,21 @@
           #["", "", ""]]
 
 posiciones = ((0,0), (0,1), (0,2), (1,0), (1,1), (1,2), (2,0), (2,1), (2,2))
-
-matriz=[]
-def creacionmatriz():
-        fila=[]
-        for i in range(0,3): #linea
-                for j in range(0,3): #columna
+   
+lab=[]
+def mazecreation():
+        maze = []
+        for i in range(0,3): #line
+                for j in range(0,3): #column
                                       
-                    if tuple([i,j]) in posiciones:
-                        fila.append(" ")
-                    else:
-                        fila.append("a")
-                matriz.append(fila)
-                matriz=[]
-creacionmatriz()
+                        if tuple([i,j]) in posiciones:
+                                maze.append("X")
+                        else:
+                                maze.append(" ")
+                lab.append (maze)
+                maze = []
+mazecreation()
  
-for x in matriz:
+for x in lab:
     print(",".join(x))
-    
-
 
