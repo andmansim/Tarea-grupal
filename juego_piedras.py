6a307aafc64760d4c4f2fdf376b68a43580c566b
andmansim <91722324+andmansim@users.noreply.github.com>
2021-12-03 18:23:42 +0100

    transformar a función

--- juego_piedras.py
+++ juego_piedras.py
@@ -9,16 +9,31 @@ Siempre empezará el jugador uno.
   
 """
 import random
-piedras = random.randint(1,8)
+piedras = random.randint(1,9)
 print(piedras)
 print ("Empieza jugando el jugardo 1")
+def repartir(piedras):
+    if piedras >= 5:
+        jugador = 5
+    elif piedras < 5 or piedras >= 3:
+        jugador = 3
+    elif piedras == 2: 
+        jugador = 2
+    else:
+        jugador = 0
+    return jugador
+
+print("Piedras del jugador 1:")
+jugador1 = repartir(piedras)
+print("Piedras del jugador 2")
+
 
 while piedras >=5:
     print("Jugador 1 coge 5 piedras")
     piedras_totales = piedras - 5
     print(piedras_totales)
     if piedras_totales == 5:
-        print("Jugador 1 gana")
+        print("Jugador 2 gana")
     elif piedras_totales == 3:
         print("Jugador 2 coge 3 piedras")
         piedras_totales = piedras_totales - 3
