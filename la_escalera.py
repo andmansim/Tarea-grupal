7004747d1b93744896d871948af71eb331a27159
andmansim <91722324+andmansim@users.noreply.github.com>
2021-12-03 14:14:52 +0100

    modificado y terminado

--- la_escalera.py
+++ la_escalera.py
@@ -8,30 +8,10 @@ lo que hay que conseguir:
 ... hasta n
 """
 import random
-#n = random.randint(0,10)
-#print (n)
-
-m = 3
-for b in range(0,3):
-    if m!=b:
-        suma ='#' * b
-        print(suma)
+m = random.randint(1,10)
+for b in range(0,m+1):
+    suma ='#' * b
+    print(suma)
     
     
-    if m == b:
-        suma1 = '#' * m
-        print(suma1)
-        
-
-    
-
-"""
-if n != 0:
-    print ('#' * (n-n))
-if n != 1:
-    print ('#' * (n-(n-1)))
-if n != 2:
-    print ('#' * (n-(n-2)))
-if n == 3:
-    print ('#' * (n-(n-3))) """
-
+      
