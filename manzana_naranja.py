9baf726251ba2d5cb7a63cf0374f5d751173bf86
andmansim <91722324+andmansim@users.noreply.github.com>
2021-12-14 17:26:28 +0100

    funciones manzana, naranja y listas

--- manzana_naranja.py
+++ manzana_naranja.py
@@ -9,6 +9,8 @@ b = (15)
 a = (5)
 distancia_casa = [s,t]
 distancia_arboles = [a,b]
+manzanas_lista = []
+naranjas_lista = []
 
 def lanzamiento_manzana():
     j = random.randint(0,11)
@@ -19,14 +21,33 @@ def lanzamiento_naranja():
     return h
 
 
-distancia_manzana = lanzamiento_manzana()
+posicion_manzana = lanzamiento_manzana()
 
-distancia_naranja = lanzamiento_naranja()
+posicion_naranja = lanzamiento_naranja()
 
-print("Distancia de una manzana:")
-print(distancia_manzana)
-print("Distancia de una naranja:")
-print(distancia_naranja)
+print("Posición de una manzana:")
+print(posicion_manzana)
+print("Posición de una naranja:")
+print(posicion_naranja)
+
+def manzana ():
+    if posicion_manzana < 5:
+        distancia_manzana = a - posicion_manzana
+    elif posicion_manzana > 5:
+        distancia_manzana = posicion_manzana - a
+    else:
+        distancia_manzana = 0
+    return distancia_manzana
+
+def naranja ():
+    if posicion_naranja < 15:
+        distancia_naranja = b - posicion_naranja
+    elif posicion_naranja > 15:
+        distancia_naranja = posicion_naranja - b
+    else:
+        distancia_naranja = 0
+    return distancia_naranja
+
 
 #objetivo: print(numero de manzanas que caen dentro de la casa)
          # print(numero de naranjas que caen dentro de la casa)
