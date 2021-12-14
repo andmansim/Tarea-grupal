728457b1c8bcca0b1e3e4bd132daea1092ec3813
andmansim <91722324+andmansim@users.noreply.github.com>
2021-12-14 17:50:49 +0100

    ordenar codigo

--- manzana_naranja.py
+++ manzana_naranja.py
@@ -21,41 +21,37 @@ def lanzamiento_naranja():
     return h
 
 
-posicion_manzana = lanzamiento_manzana()
 
-posicion_naranja = lanzamiento_naranja()
 
-print("Posición de una manzana:")
-print(posicion_manzana)
-print("Posición de una naranja:")
-print(posicion_naranja)
-
-def manzana ():
+def manzana (posicion_manzana):
     if posicion_manzana != 5:
         distancia_manzana = posicion_manzana - a
     else:
         distancia_manzana = 0
     return distancia_manzana
 
-def naranja ():
+def naranja (posicion_naranja):
     if posicion_naranja != 15:
         distancia_naranja = posicion_naranja - b
     else:
         distancia_naranja = 0
     return distancia_naranja
 
-m = manzana()
+
+posicion_m = lanzamiento_manzana()
+posicion_n = lanzamiento_naranja()
+m = manzana(posicion_m)
 manzanas_lista.append(m)
 print(manzanas_lista)
-n = naranja()
+n = naranja(posicion_n)
 naranjas_lista.append(n)
 print(naranjas_lista)
 total_manzanas = 0
-if 7 < posicion_manzana < 11:
+if 7 < posicion_m < 11:
     total_manzanas = total_manzanas + 1
     print(total_manzanas)
 
 total_naranjas = 0
-if 7 < posicion_naranja < 11:
+if 7 < posicion_n < 11:
     total_naranjas = total_naranjas + 1  
     print(total_naranjas)
