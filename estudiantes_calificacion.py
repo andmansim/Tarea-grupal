fe9e10bb8323804676db390e72ac75145e98e0c2
andmansim <91722324+andmansim@users.noreply.github.com>
2021-12-14 16:43:01 +0100

    archivo para pruebas e intento 2

--- estudiantes_calificacion.py
+++ estudiantes_calificacion.py
@@ -9,9 +9,12 @@ def calificaciones():
     notas.append(calificacion)
     
 # Para conseguir un múltiplo de 5 mayor que la nota
-def redondear(num, divisor):
-    a = int((num/divisor + 0.5) * divisor )
+def redondear(x, base):
+    a = base * round(x/base)
+    if a < x:
+        a = a + 5
     return a
+
 
     
 y = 0
@@ -25,17 +28,29 @@ notas.sort()
 print(notas)
 suspensos = []
 aprobados = []
+
 for i in range (0,5):
     if notas[i] < 40:
         suspensos.append(notas[i])
     else:
         aprobados.append(notas[i])
-        
+    
 print(suspensos)
 print(aprobados)
-for j in range(len(aprobados)):
-    h = aprobados[j]
-    u = redondear(aprobados[j], 5)
-    resta = h - u
+
+
+def resta(h,u):
+   restas = h - u
+   
+   return restas
+
+for j in aprobados:
+    b = redondear(aprobados[j],5)
+    resta(aprobados[j], b)
     print(resta)
 
+'''contador = 0
+while contador != 6 or aprobados == []:
+    print(myround(aprobados[0],5))
+    aprobados.pop(0)
+    contador = contador + 1'''
