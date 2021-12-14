ca0f4a2351eceb7b12b22907d8c36912c559e43c
andmansim <91722324+andmansim@users.noreply.github.com>
2021-12-14 13:48:11 +0100

    suspensos

--- estudiantes_calificacion.py
+++ estudiantes_calificacion.py
@@ -1,15 +1,13 @@
 import random
 
 notas = []
-mulriplos_5 = []
+mulriplos_5 = [45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100]
 
 def calificaciones():
     
     calificacion = random.randint(0,100)
     notas.append(calificacion)
-    
-    
-    return 
+ 
     
     
 y = 0
@@ -17,8 +15,15 @@ while y < 5:
 
     d = calificaciones()
     y = y + 1
+print("Notas iniciales")
 print(notas)
-
-
-
+notas.sort()
+print(notas)
+suspensos = []
+for i in range (0,5):
+    if notas[i] < 40:
+        suspensos.append(notas[i])
+    else:
+        print("b")
+print(suspensos)
 
